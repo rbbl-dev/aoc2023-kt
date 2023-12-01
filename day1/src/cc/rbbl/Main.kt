c95f84649d59cd983051947f96ba7cf491d32dd5
rbbl-dev <dev@rbbl.cc>
2023-12-01 13:54:02 +0100

    day1 part2

--- day1/src/cc/rbbl/Main.kt
+++ day1/src/cc/rbbl/Main.kt
@@ -1,12 +1,58 @@
 package cc.rbbl
 
 suspend fun main() {
-  println(day1(fetchPuzzleInput(1)))
+  val input = fetchPuzzleInput(1)
+  println(part1(input))
+  println(part2(input))
 }
 
-fun day1(input: String): Int {
+fun part1(input: String): Int {
   return input.trim().lines().sumOf { line ->
     val numbers = line.replace(Regex("\\D"), "")
     "${numbers.first()}${numbers.last()}".toInt()
   }
 }
+
+fun part2(input: String): Int {
+  return input.trim().lines().sumOf { line ->
+    val numbers = line.getFirstAnLastNumber()
+    "${numbers.first}${numbers.second}".toInt()
+  }
+}
+
+fun String.getFirstAnLastNumber(): Pair<Int, Int> {
+  val firstTextNumber = this.findAnyOf(wordNumberMap.keys)
+  val firstActualNumber = this.findAnyOf(wordNumberMap.values)
+  val lastTextNumber = this.findLastAnyOf(wordNumberMap.keys)
+  val lastActualNumber = this.findLastAnyOf(wordNumberMap.values)
+
+  val firstNumber = if(firstActualNumber != null && firstTextNumber != null) {
+    if(firstActualNumber.first < firstTextNumber.first) {
+      firstActualNumber.second.toInt()
+    }else {
+      wordNumberMap[firstTextNumber.second]!!.toInt()
+    }
+  }else firstActualNumber?.second?.toInt() ?: wordNumberMap[firstTextNumber!!.second]!!.toInt()
+
+  val lastNumber = if(lastActualNumber != null && lastTextNumber != null) {
+    if(lastActualNumber.first > lastTextNumber.first) {
+      lastActualNumber.second.toInt()
+    }else {
+      wordNumberMap[lastTextNumber.second]!!.toInt()
+    }
+  }else lastActualNumber?.second?.toInt() ?: wordNumberMap[lastTextNumber!!.second]!!.toInt()
+
+  return Pair(firstNumber, lastNumber)
+}
+
+val wordNumberMap = mapOf(
+  "one" to "1",
+  "two" to "2",
+  "three" to "3",
+  "four" to "4",
+  "five" to "5",
+  "six" to "6",
+  "seven" to "7",
+  "eight" to "8",
+  "nine" to "9"
+)
--- day1/test/cc/rbbl/Test.kt
+++ day1/test/cc/rbbl/Test.kt
@@ -4,14 +4,40 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Test {
-    val input ="""
+    val input1 ="""
         1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet
     """.trimIndent()
+
     @Test
-    fun day1Test() {
-        assertEquals(142, day1(input))
+    fun part1Test() {
+        assertEquals(142, part1(input1))
+    }
+
+    val input2 = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent()
+
+    @Test
+    fun part2Test() {
+        assertEquals(281, part2(input2))
+    }
+
+    @Test
+    fun getFirstAnLastNumber() {
+        assertEquals(Pair(7, 9), "abseveninedef".getFirstAnLastNumber())
+    }
+
+    @Test
+    fun getFirstAnLastNumber2() {
+        assertEquals(Pair(3, 6), "rtkrbtthree8sixfoureight6".getFirstAnLastNumber())
     }
 }
